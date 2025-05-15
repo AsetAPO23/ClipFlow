70a757ef3ce14a7409ecaf0ec4817c656b36c44d
AsetAPO23 <rae1209go@gmail.com>
2025-05-15 06:23:28 +0500

    404.js

--- 404.js
+++ 404.js
@@ -1,8 +1,8 @@
-// Скрипт для переключения темы и сохранения выбора пользователя
+
 document.addEventListener('DOMContentLoaded', () => {
   const body = document.body;
 
-  // Проверяем сохранённую тему
+  
   const savedTheme = localStorage.getItem('theme');
   if (savedTheme === 'dark-theme') {
     body.classList.add('dark-theme');
